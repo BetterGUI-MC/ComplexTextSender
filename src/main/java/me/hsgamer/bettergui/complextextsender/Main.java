a32349d1edc476ea5eeec3c00435db8821757300
HSGamer <huynhqtienvtag@gmail.com>
2020-04-26 20:58:49 +0700

    remove it... will find a better solution

--- src/main/java/me/hsgamer/bettergui/complextextsender/Main.java
+++ src/main/java/me/hsgamer/bettergui/complextextsender/Main.java
@@ -19,8 +19,5 @@ public final class Main extends Addon {
   public void onEnable() {
     CommandBuilder.register("json:", JSONCommand.class);
     CommandBuilder.register("json-?broadcast:", JSONBroadcastCommand.class);
-    CommandBuilder.register("json-?action", JSONActionBarCommand.class);
-    CommandBuilder.register("json-?action-?broadcast", JSONActionBarBroadcastCommand.class);
-    CommandBuilder.register("json-?broadcast-?action", JSONActionBarBroadcastCommand.class);
   }
 }
